730bc8be0235a6f829e0c8c69605e5067686cf37
bfsalamin <brunofrancosalamin@gmail.com>
2022-07-27 16:05:33 -0500

    ultimo commit

--- Main.java
+++ Main.java
@@ -13,15 +13,16 @@ public class Main {
                 .join();
     }
 
-    static URI uri_build() {
-        UriBuild uri_loc = new UriBuild("http://3.138.208.231:9041/api/advisers/cliente/?cedula=3-761-2397&edad=24");
+    static URI uri_build(String baseuri) {
+        UriBuild uri_loc = new UriBuild(baseuri);
         uri_loc.addquery("cedula", "3-761-2397");
         return URI.create(uri_loc.render());
     }
 
     static HttpRequest request_build(){
+        String apiuri = "http://3.138.208.231:9041/api/advisers/cliente/";
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(Main.uri_build())
+                .uri(Main.uri_build(apiuri))
                 .GET()
                 .build();
         return request;
